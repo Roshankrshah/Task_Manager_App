b58af8408d6655319abd51d700131c2384a26f8b
Roshan <shahroshan0720@gmail.com>
2023-06-13 10:50:34 +0530

    app.js and route setup

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express(); 
+const tasks = require('./routes/task');
+
+//middleware
+app.use(express.json());
+
+
+const port = 3000;
+
+app.get('/hello',(req,res)=>{
+    res.send("Task manager");
+});
+
+app.use('/api/v1/tasks',tasks);
+
+app.listen(port,console.log(`server is listening on https://localhost:${port}`));
