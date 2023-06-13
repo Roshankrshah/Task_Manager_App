85610f5b84590326df1bc8c34261e2a3630dc1d5
Roshan <shahroshan0720@gmail.com>
2023-06-13 10:59:21 +0530

    add controllers folder

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express(); 
-const tasks = require('./routes/task');
+const tasks = require('./routes/tasks');
 
 //middleware
 app.use(express.json());
