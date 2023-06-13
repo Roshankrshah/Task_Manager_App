19663d1408de75448e0c0b6eaa345a9c59171f4e
Roshan <shahroshan0720@gmail.com>
2023-06-13 13:18:58 +0530

    setting up the env file

--- db/connect.js
+++ db/connect.js
@@ -1,11 +1,7 @@
 const { default: mongoose } = require("mongoose")
 
-const connectionString = 
-'mongodb+srv://roshah07:1234@cluster0.lgzhj6b.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority'
-
-
 const connectDB = (url) =>{
-    return mongoose.connect(connectionString)
+    return mongoose.connect(url)
 }
 
 module.exports = connectDB
